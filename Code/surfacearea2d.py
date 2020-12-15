a0379e109c1514760aafb9110c7691d100db9a0b
ApplePy39 <73040360+ApplePy39@users.noreply.github.com>
2020-12-15 09:54:57 -0600

    added changes to surfacearea2d.py

--- Code/surfacearea2d.py
+++ Code/surfacearea2d.py
@@ -10,14 +10,14 @@ class Thumnbail(Scene):
         ThumnbailContentII = TextMobject("Of 2D")
         ThumnbailContentIII = TextMobject("Objects").move_to(DOWN*0.75)
 
-        ThumnbailGroup = VGroup(ThumnbailContent, ThumnbailContentII, ThumnbailContentIII).scale(3).set_color(color=[RED_C, GREEN_C])
+        ThumnbailGroup = VGroup(ThumnbailContent, ThumnbailContentII, ThumnbailContentIII).scale(3).set_color(color=[BLUE_C, PURPLE_C])
 
         self.add(ThumnbailGroup)
 
 class SquareArea(Scene):
     def construct(self):
 
-        SquareWords = TextMobject("Surface area of a Square").set_color(color=[RED_C, GREEN_C]).scale(2)
+        SquareWords = TextMobject("Surface area of a Square").set_color(color=[BLUE_C, PURPLE_C]).scale(2)
 
         self.play(Write(SquareWords))
         self.wait(2)
@@ -29,8 +29,25 @@ class SquareArea(Scene):
         self.play(ShowCreation(SquareDiagram), run_time=2)
         self.wait()
 
-        self.play(SquareDiagram.move_to, UP)
+        self.play(SquareDiagram.move_to, UP) 
 
-        ImagineSquare = TextMobject("Let's imagine this square is 5 feet wide.").scale(1.25).set_color(color=[RED_C, GREEN_C])
+        ImagineSquare = TextMobject("Let's imagine this square is 5cm wide.").scale(1.25).set_color(color=[BLUE_C, PURPLE_C]).move_to(DOWN*2)
 
+        self.play(Write(ImagineSquare))
+        self.wait(2)
         
+        self.play(FadeOutAndShiftDown(ImagineSquare))
+
+        self.play(SquareDiagram.move_to, DOWN*0.25)
+
+        FiveCMWide = TextMobject("5cm").scale(0.85).move_to(DOWN*2).set_color(color=[ORANGE, PINK])
+        
+        self.play(Write(FiveCMWide))
+        self.wait()
+
+        self.play(
+            SquareDiagram.move_to, LEFT*3,
+            FiveCMWide.move_to, LEFT*3+DOWN*1.5
+        )
+
+        self.wait()
